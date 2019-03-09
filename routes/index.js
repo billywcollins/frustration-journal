6939b052dfa3bb2a97c2e76c246c2a74cb6c3faa
Billy Collins <billy.collins86@gmail.com>
2019-03-09 09:44:47 -0600

    severa updates

--- routes/index.js
+++ routes/index.js
@@ -18,152 +18,6 @@ router.get('/', function(req, res, next) {
 	// after you finish this, you'll need to update the post request for adding entries
 });
 
-router.get('/journal', ensureAuthenticated, function(req, res, next) {
-  //console.log(req.user);
-
-  async.parallel({
-  	entries: function(callback) {
-  		Entry.find({'user': req.user._id}).populate('category').exec(callback);
-  	}, 
-  	categories: function(callback) {
-  		Category.find({'user': req.user._id}).exec(callback)
-  	}, 
-  	}, function(err, results) {
-  		if (err) { return next(err); }
-  		//console.log(results.entries)
-  		res.render('journal', { title: 'Frustrations Journal', entries: results.entries, categories: results.categories, filtered: false });
-	});
-});
-
-// Get a single entry's details
-
-router.get('/journal/:id', ensureAuthenticated, function(req, res, next) {
-	Entry.findById(req.params.id)
-	.populate('category')
-	.exec(function(err, entry){
-		if (err) { return next(err); }
-		//console.log(entry)
-		res.render('single-entry', {title: 'Entry Details', entry: entry})
-	})
-})
-
-router.post('/delete', ensureAuthenticated, function(req, res, next) {
-	Entry.findByIdAndRemove(req.body.entryid, function(err) {
-		if (err) { return next(err);}
-		req.flash('success', 'Your entry was successfully deleted.')
-		res.redirect('/journal');
-	})
-})
-
-// Filter a user's entries by category
-
-router.post('/journal', ensureAuthenticated, function(req, res, next) {
-	var categories = req.body.category;
-	//console.log(categories);
-
-	async.parallel({
-  	entries: function(callback) {
-  		Entry.find({'user': req.user._id, 'category': {$in: categories}}).populate('category').exec(callback);
-  	}, 
-  	categories: function(callback) {
-  		Category.find({'user': req.user._id}).exec(callback)
-  	}, 
-  	}, function(err, results) {
-  		if (err) { return next(err); }
-  		console.log(results.entries);
-  		res.render('journal', { title: 'Frustrations Journal', entries: results.entries, categories: results.categories, filtered: true });
-	});
-})
-
-// Add a category (from the add new entry form)
-
-router.post('/add-category', ensureAuthenticated, 
-	[
-		check('name', 'Your category needs a name.').isLength({min:1}).trim()
-	], 
-	function(req, res, next) {
-		const errors = validationResult(req);
-
-		var category = new Category({
-			name: req.body.name,
-			user: req.user._id
-		});
-
-		if(!errors.isEmpty()) {
-			req.flash('failure', 'You category wasn\'t added. Try again.')
-			res.redirect('/add-entry');
-			return;
-		} else {
-			category.save(function (err) {
-				if(err) {
-					return next(err);
-				}
-				//req.flash('success', 'Entry created.')
-				res.redirect('/add-entry');
-			})
-		}
-
-})
-
-router.get('/add-entry', ensureAuthenticated, function(req, res, next) {
-	Category.find({user: req.user._id}).exec(function(err, categories) {
-		if (err) { return handleError(err); }
-		res.render('add-entry', {title: 'Add Journal Entry', categories: categories});
-	})
-})
-
-function ensureAuthenticated(req, res, next) {
-	if(req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect('/login');
-}
-
-router.post('/', 
-	[
-		check('title', 'Please enter a title.').isLength({min:1}).trim(),
-		check('location', 'Please enter a location.').isLength({min:1}).trim(),
-		check('description', 'Please enter a description.').isLength({min:1}).trim(),
-		check('idea', 'Please enter an idea.').isLength({min:1}).trim(),
-		check('category', 'Choose a cateogry.').isMongoId()
-	], 
-	function(req, res, next) {
-		const errors = validationResult(req);
-		var date = moment().format('MMM D, YYYY');
-		var user = req.user._id;
-		//console.log(req.body);
-
-		var entry = new Entry({
-			title: req.body.title,
-			location: req.body.location,
-			description: req.body.description,
-			category: req.body.category,
-			idea: req.body.idea,
-			size: req.body.size,
-			date: date,
-			user: user
-		});
-
-		if(!errors.isEmpty()) {
-			Category.find({user: req.user._id}).exec(function(err, categories) {
-				if (err) { return handleError(err); }
-				res.render('add-entry', {title: 'Add Journal Entry', categories: categories, entry: entry, errors:errors.array()});
-				//console.log(entry);
-			})
-			//res.render('add-entry', {title: 'Add Entry', errors:errors.array()});
-			return;
-		} else {
-			entry.save(function(err) {
-				if(err) {
-					return next(err);
-				}
-				req.flash('success', 'Entry created.')
-				res.redirect('/journal');
-			})
-		}
-
-})
-
 //** REGISTER AND AUTH **//
 
 router.get('/login', function(req, res, next) {
@@ -259,6 +113,152 @@ router.get('/logout', function(req, res) {
 	res.redirect('/login');
 });
 
+router.get('/journal', ensureAuthenticated, function(req, res, next) {
+  //console.log(req.user);
+
+  async.parallel({
+  	entries: function(callback) {
+  		Entry.find({'user': req.user._id}).populate('category').exec(callback);
+  	}, 
+  	categories: function(callback) {
+  		Category.find({'user': req.user._id}).exec(callback)
+  	}, 
+  	}, function(err, results) {
+  		if (err) { return next(err); }
+  		//console.log(results.entries)
+  		res.render('journal', { title: 'Frustrations Journal', entries: results.entries, categories: results.categories, filtered: false });
+	});
+});
+
+// Get a single entry's details
+
+router.get('/journal/:id', ensureAuthenticated, function(req, res, next) {
+	Entry.findById(req.params.id)
+	.populate('category')
+	.exec(function(err, entry){
+		if (err) { return next(err); }
+		//console.log(entry)
+		res.render('single-entry', {title: 'Entry Details', entry: entry})
+	})
+})
+
+router.post('/delete', ensureAuthenticated, function(req, res, next) {
+	Entry.findByIdAndRemove(req.body.entryid, function(err) {
+		if (err) { return next(err);}
+		req.flash('success', 'Your entry was successfully deleted.')
+		res.redirect('/journal');
+	})
+})
+
+// Filter a user's entries by category
+
+router.post('/journal', ensureAuthenticated, function(req, res, next) {
+	var categories = req.body.category;
+	//console.log(categories);
+
+	async.parallel({
+  	entries: function(callback) {
+  		Entry.find({'user': req.user._id, 'category': {$in: categories}}).populate('category').exec(callback);
+  	}, 
+  	categories: function(callback) {
+  		Category.find({'user': req.user._id}).exec(callback)
+  	}, 
+  	}, function(err, results) {
+  		if (err) { return next(err); }
+  		console.log(results.entries);
+  		res.render('journal', { title: 'Frustrations Journal', user:req.user, entries: results.entries, categories: results.categories, filtered: true });
+	});
+})
+
+// Add a category (from the add new entry form)
+
+router.post('/add-category', ensureAuthenticated, 
+	[
+		check('name', 'Your category needs a name.').isLength({min:1}).trim()
+	], 
+	function(req, res, next) {
+		const errors = validationResult(req);
+
+		var category = new Category({
+			name: req.body.name,
+			user: req.user._id
+		});
+
+		if(!errors.isEmpty()) {
+			req.flash('failure', 'You category wasn\'t added. Try again.')
+			res.redirect('/add-entry');
+			return;
+		} else {
+			category.save(function (err) {
+				if(err) {
+					return next(err);
+				}
+				//req.flash('success', 'Entry created.')
+				res.redirect('/add-entry');
+			})
+		}
+
+})
+
+router.get('/add-entry', ensureAuthenticated, function(req, res, next) {
+	Category.find({user: req.user._id}).exec(function(err, categories) {
+		if (err) { return handleError(err); }
+		res.render('add-entry', {title: 'Add Journal Entry', categories: categories});
+	})
+})
+
+function ensureAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+}
+
+router.post('/', 
+	[
+		check('title', 'Please enter a title.').isLength({min:1}).trim(),
+		check('location', 'Please enter a location.').isLength({min:1}).trim(),
+		check('description', 'Please enter a description.').isLength({min:1}).trim(),
+		check('idea', 'Please enter an idea.').isLength({min:1}).trim(),
+		check('category', 'Choose a cateogry.').isMongoId()
+	], 
+	function(req, res, next) {
+		const errors = validationResult(req);
+		var date = moment().format('MMM D, YYYY');
+		var user = req.user._id;
+		//console.log(req.body);
+
+		var entry = new Entry({
+			title: req.body.title,
+			location: req.body.location,
+			description: req.body.description,
+			category: req.body.category,
+			idea: req.body.idea,
+			size: req.body.size,
+			date: date,
+			user: user
+		});
+
+		if(!errors.isEmpty()) {
+			Category.find({user: req.user._id}).exec(function(err, categories) {
+				if (err) { return handleError(err); }
+				res.render('add-entry', {title: 'Add Journal Entry', categories: categories, entry: entry, errors:errors.array()});
+				//console.log(entry);
+			})
+			//res.render('add-entry', {title: 'Add Entry', errors:errors.array()});
+			return;
+		} else {
+			entry.save(function(err) {
+				if(err) {
+					return next(err);
+				}
+				req.flash('success', 'Entry created.')
+				res.redirect('/journal');
+			})
+		}
+
+})
+
 //** PROFILE **//
 
 router.get('/profile', ensureAuthenticated, function(req, res, next) {
@@ -266,23 +266,21 @@ router.get('/profile', ensureAuthenticated, function(req, res, next) {
 	res.render('profile', {title: 'My Profile', username: username})
 })
 
-router.get('/profile/change-email', ensureAuthenticated, function(req, res, next) {
-	var username = req.user.username;
-	res.render('change-email', {title: 'Change Email Address', username:username})
-})
+// router.get('/profile/change-email', ensureAuthenticated, function(req, res, next) {
+// 	var username = req.user.username;
+// 	res.render('change-email', {title: 'Change Email Address', username:username})
+// })
 
-router.post('/profile/change-email', [
+router.post('/profile/change-email', ensureAuthenticated, [
 	check('new_username', 'Please enter a valid email address.').isLength({min:1}).isEmail().trim(),
-	check('match_new_username', 'This should match your new email address.').equals('new_username')
-	], 
-	ensureAuthenticated, function(req, res, next) {
+	], function(req, res, next) {
 		const errors = validationResult(req);
 
 		var username = req.user.username;
 		var updated_username = req.body.new_username
 
 		if(!errors.isEmpty()) {
-			res.render('change-email', {title: 'Change Email Address', errors:errors.array(), username: username});
+			res.render('profile', {title: 'My Profile', errors:errors.array(), username: username, user:req.user});
 			return;
 		} else {
 			User.findOneAndUpdate({username: username}, {username: updated_username}, {}, function(err, result){
@@ -290,19 +288,18 @@ router.post('/profile/change-email', [
 					return next(err);
 				}	
 				req.flash('success', 'Success! You changed your email address.')
-				res.redirect('/profile');
+				res.redirect('/journal');
 			})
 		}
 	}
 )
 
-router.get('/profile/change-password', ensureAuthenticated, function(req, res, next) {
-	res.render('change-password', {title: 'Change Password'})
-})
+// router.get('/profile/change-password', ensureAuthenticated, function(req, res, next) {
+// 	res.render('change-password', {title: 'Change Password'})
+// })
 
 router.post('/profile/change-password', ensureAuthenticated, [
 		check('newPassword', 'Your password should be at least 6 characters long.').isLength({min:6}),
-		//check('newPassword2', 'This should match your new password.').equals('newPassword')
 	], function(req, res, next) {
 		const errors = validationResult(req);
 
@@ -314,12 +311,10 @@ router.post('/profile/change-password', ensureAuthenticated, [
 		var hash_new_password = bcrypt.hashSync(req.body.newPassword, salt);
 
 		if(!errors.isEmpty()) {
-			//console.log(new_password)
-			//console.log(match_new_password)
-			res.render('change-password', {title: 'Change Password', errors:errors.array()});
+			res.render('profile', {title: 'My Profile', errors:errors.array()});
 			return;
 		} else if (new_password !== match_new_password) {
-			res.render('change-password', {title: 'Change Password', error_type: 'password_match', message: 'This should match your new password.'});
+			res.render('profile', {title: 'My Profile', error_type: 'password_match', message: 'This should match your new password.'});
 		} else {
 			User.update({username: username}, {password: hash_new_password}, {}, function(err, result){
 				if(err) {return next(err);}
@@ -327,23 +322,7 @@ router.post('/profile/change-password', ensureAuthenticated, [
 				req.flash('success', 'Success! You changed your password. Please log in again.');
 				res.redirect('/login');
 			})
-			// bcrypt.compare(req.body.password, req.user.password, function(err, res) {
-			// 	if(err) {return next(err);}
-			// 	if(res) {
-			// 		User.update({username: username}, {password: hash_new_password}, {}, function(err, result){
-			// 			if(err) {return next(err);}
-			// 			req.logout();
-			// 			req.flash('success', 'Success! You changed your password. Please log in again.');
-			// 			res.redirect('/login');
-			// 		})
-			// 	}
-			// })
 		}
-		// else if (hash_current_password !== req.user.password) {
-		// 	console.log(req.user.password)
-		// 	console.log(hash_current_password)
-		// 	res.render('change-password', {title: 'Change Password', error_type: 'incorrect_password', message: 'Your password is incorrect.'});
-		// } 
 })
 
 router.get('/.well-known/acme-challenge/:content', function(req, res) {
